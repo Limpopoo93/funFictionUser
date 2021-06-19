8aa8bd5924c3578f1ea6522752dba3f978bafdb8
attiq <marachckow2013@yandex.ru>
2021-06-19 12:35:53 +0300

    heroku deploy

--- src/main/java/FunFictionUserProject/funFictionUser/FunFictionUserApplication.java
+++ src/main/java/FunFictionUserProject/funFictionUser/FunFictionUserApplication.java
@@ -2,12 +2,24 @@ package FunFictionUserProject.funFictionUser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+@Controller
 @SpringBootApplication
 public class FunFictionUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FunFictionUserApplication.class, args);
+	}
+	@RequestMapping("/")
+	String index() {
+		return "main";
 	}
 
 }
